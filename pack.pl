5e1816391ff560ff26d80e89be4bd8c25e5c7ea0
James N. V. Cash <james.nvc@gmail.com>
2018-03-10 10:33:30 -0500

    Bump pack version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(uuid).
 title('Generating and parsing version 4 UUIDs').
-version('1.0.0').
+version('1.0.1').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/uuid').
 download('https://github.com/jamesnvc/uuid/releases/*.zip').
