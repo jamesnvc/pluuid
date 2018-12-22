a11fa9a5b457acfb29f75221bc2c7558c20e9c28
James N. V. Cash <james.nvc@gmail.com>
2018-12-22 13:24:35 -0500

    Bump pack version after rename

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(pluuid).
 title('Generating and parsing version 4 UUIDs in Pure Prolog').
-version('1.1.1').
+version('2.0.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/pluuid').
 download('https://github.com/jamesnvc/pluuid/releases/*.zip').
